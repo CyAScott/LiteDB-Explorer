093817c5b38a08eb654ca4fd92ca83b48057d109
Cy Scott <cy.a.scott@live.com>
2017-02-23 18:29:29 -0500

    setup projects

---
+++ src/LiteDB.Explorer.Windows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiteDB.Explorer.Windows
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}

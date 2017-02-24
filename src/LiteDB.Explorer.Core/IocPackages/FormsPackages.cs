1c0591d3eec46219ecb1ea134c09977e08c4e3b0
Cy Scott <cy.a.scott@live.com>
2017-02-23 20:45:31 -0500

    added installer for forms

---
+++ src/LiteDB.Explorer.Core/IocPackages/FormsPackages.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace LiteDB.Explorer.Core.IocPackages
+{
+    /// <summary>
+    /// Registers all the forms and view models in the IOC container.
+    /// </summary>
+    public class FormsPackages : IPackage
+    {
+        /// <summary>
+        /// Registers all the forms and view models in the IOC container.
+        /// </summary>
+        public void RegisterServices(Container container)
+        {
+            foreach (var type in typeof(FormsPackages)
+                .Assembly
+                .GetExportedTypes()
+                .Where(type => type.Namespace?.StartsWith("LiteDB.Explorer.Core.Forms") ?? false))
+            {
+                container.Register(type, type, Lifestyle.Singleton);
+            }
+        }
+    }
+}
